2dd3046d55805a1ce71c7e97e05677494f729c79
ikealmighty <isaacyates7@gmail.com>
2025-04-15 18:05:46 -0500

    fixed some seed file errors

--- Develop/server/src/seeds/seed.ts
+++ Develop/server/src/seeds/seed.ts
@@ -2,13 +2,19 @@ import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from "./pythonQuestions.json" assert { type: "json" };
+import pythonQuestions from "./pythonQuestions.json" with { type: "json" };
 
 db.once("open", async () => {
-  await cleanDB("Question", "questions");
+  try {
+    await cleanDB("Question", "questions");
+    console.log(pythonQuestions);
+    const resp = await Question.insertMany(pythonQuestions);
 
-  await Question.insertMany(pythonQuestions);
+    console.log(resp)
 
-  console.log("Questions seeded!");
+    console.log("Questions seeded!");
+  } catch (err) {
+    console.log(err);
+  }
   process.exit(0);
 });
